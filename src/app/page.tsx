7b294ad1c5638e232be883c8633dd85b681241f1
Astjan Selimaj <info@selimaj.nl>
2025-11-12 16:48:47 +0100

    Add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import nextjsLogo from "@/../public/nextjs.svg";
 import typescriptLogo from "@/../public/typescript.svg";
 import reactLogo from "@/../public/react.svg";
 import mongodbLogo from "@/../public/mongodb.svg";
 import SkillsList from "./components/SkillsList";
+import { motion } from "framer-motion";
 
 const skills: [string, string][] = [
 	["Next.js", nextjsLogo],
@@ -14,10 +17,15 @@ const skills: [string, string][] = [
 
 export default function Home() {
 	return (
-		<div className="flex-col">
+		<motion.div
+			className="flex-col transition-color transition-discrete duration-initial"
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.8, ease: "easeOut", delay: 0.5 }}
+		>
 			<h1 className="text-6xl font-light mb-1.5">Astjan Selimaj</h1>
 			<p className="text-2xl mb-3.5">Full Stack Web Developer</p>
 			<SkillsList skills={skills} />
-		</div>
+		</motion.div>
 	);
 }
